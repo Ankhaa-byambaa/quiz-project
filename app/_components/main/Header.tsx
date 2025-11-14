1a6eacc84130eb6569b37578127a2d7a5ee88254
25LP5026 25LP5026 <25LP5026@G301-CC0200.local>
2025-11-14 09:14:31 +0800

    q

--- app/_components/main/Header.tsx
+++ app/_components/main/Header.tsx
@@ -1,13 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { UserButton } from "@clerk/nextjs";
 export function Header() {
   return (
     <>
       <div className="w-screen h-14 bg-white border border-[#E4E4E7] flex items-center justify-between px-5  fixed z-50 p-4">
         <div className=" text-6 text-black font-semibold">Quiz app</div>
-        <Avatar>
-          <AvatarImage className="w-10" src="avatarPic.jpg" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <UserButton />
       </div>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import {
   SignUpButton,
   SignedIn,
   SignedOut,
-  UserButton,
 } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -36,19 +35,17 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
+          <SignedOut>
+            <header className="flex justify-center items-center p-4 gap-4 h-screen">
               <SignInButton />
               <SignUpButton>
-                <button className="bg-[black] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <button className="bg-[black] hover:bg-gray-400 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
               </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+            </header>
+          </SignedOut>
+
           <SignedIn> {children}</SignedIn>
         </body>
       </html>
